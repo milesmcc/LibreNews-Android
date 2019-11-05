0cc9bc7d1c735034c1175399cd651b8d2d329cca
jomo <git@jomo.tv>
2019-11-05 23:35:03 +0100

    Fetch channels from server
    This adds
      1. an `available_channels` preference
      2. a DoneCallback for the FlashManager
    
    The SettingsActivityFragment populates the `channels` list from the `available_channels`, which is updated whenever the FlashRetreiver fetches the API.
    The API is also fetched when the `server_url` is changed and the callback is used to update the channels list.

--- LibreNews-App/src/main/java/app/librenews/io/librenews/views/SettingsActivityFragment.java
+++ LibreNews-App/src/main/java/app/librenews/io/librenews/views/SettingsActivityFragment.java
@@ -1,21 +1,34 @@
 package app.librenews.io.librenews.views;
 
 import android.content.SharedPreferences;
+import android.preference.MultiSelectListPreference;
 import android.preference.PreferenceFragment;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import app.librenews.io.librenews.R;
 import app.librenews.io.librenews.controllers.FlashManager;
 import app.librenews.io.librenews.controllers.SyncManager;
 
 public class SettingsActivityFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener{
+    SharedPreferences prefs;
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.preferences);
         PreferenceManager.getDefaultSharedPreferences(getActivity()).registerOnSharedPreferenceChangeListener(this);
+        MultiSelectListPreference pref = (MultiSelectListPreference) findPreference("channels");
+        prefs = PreferenceManager.getDefaultSharedPreferences(getContext());
+        Set<String> channels = prefs.getStringSet("available_channels", new HashSet<String>());
+        CharSequence[] achannels = channels.toArray(new CharSequence[channels.size()]);
+        pref.setEntries(achannels);
+        pref.setEntryValues(achannels);
     }
 
     @Override
@@ -23,7 +36,21 @@ public class SettingsActivityFragment extends PreferenceFragment implements Shar
                                           String key) {
         System.out.println(key + " changed!");
         if(key.equals("refresh_preference") || key.equals("automatically_refresh")){
-            new SyncManager(getView().getContext(), new FlashManager(getView().getContext())).startSyncService();
+            FlashManager fmgr = new FlashManager(getView().getContext());
+            new SyncManager(getView().getContext(), fmgr).startSyncService();
+        } else if (key.equals("server_url")) {
+            FlashManager fmgr = new FlashManager(getView().getContext());
+            fmgr.addDoneCallback(new FlashManager.DoneCallback() {
+                @Override
+                public void onDone() {
+                    MultiSelectListPreference pref = (MultiSelectListPreference) findPreference("channels");
+                    Set<String> channels = prefs.getStringSet("available_channels", new HashSet<String>());
+                    CharSequence[] achannels = channels.toArray(new CharSequence[channels.size()]);
+                    pref.setEntries(achannels);
+                    pref.setEntryValues(achannels);
+                }
+            });
+            fmgr.refresh();
         }
         if (getActivity() instanceof MainFlashActivity) {
             ((MainFlashActivity)getActivity()).regenerateToolbarStatus();
